30099c99dcd8faf07bb72385e7894870519ab1b0
hecrabbs <58491968+hecrabbs@users.noreply.github.com>
2019-12-04 10:37:49 -0800

    Update game.js

--- game.js
+++ game.js
@@ -121,7 +121,7 @@ class Sprite {
   
   function preload() {
     sprite = loadImage(
-      "https://images.unsplash.com/photo-1572537165377-627a37043464?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+"https://hecrabbs.github.io/chaser-game/player.png"
     );
   }
   
